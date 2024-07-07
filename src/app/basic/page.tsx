13502a78aa31507be6b1c184bcaaeec3f23c5d61
xbiwas <biwas2059@gmail.com>
2024-07-07 14:49:58 +0545

    Basic LLM

---
+++ src/app/basic/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React, { useState } from "react";
+import { generateResponse } from "@/utils/prompt";
+import Link from "next/link";
+
+const Home = () => {
+  const [res, setRes] = useState<string | null>(null);
+
+  const displayContent = async () => {
+    const response = await generateResponse();
+    setRes(response);
+  };
+
+  return (
+    <div>
+      <h1>
+        This is a simple LLM App with built with Langchain Expression Language{" "}
+        <br />
+        new SystemMessage("Translate the following from English into Italian."){" "}
+        <br />
+        new HumanMessage("I love you!"),
+      </h1>
+      <button onClick={displayContent}>Generate</button>
+      {res && (
+        <div>
+          <h2>Response:</h2>
+          <p>{res}</p>
+        </div>
+      )}
+
+      <p>
+        Find code in my github{" "}
+        <span>
+          <a href="https://github.com/biwasbhandari" target="_blank">
+            Biwas Bhandari
+          </a>
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/utils/prompt.ts
@@ -0,0 +1,22 @@
+import { ChatOpenAI } from "@langchain/openai";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+
+const chatModel = new ChatOpenAI({
+  model: "gpt-4",
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+});
+
+const praser = new StringOutputParser();
+
+export const generateResponse = async () => {
+  const messages = [
+    new SystemMessage("Translate the following from English into Italian."),
+    new HumanMessage("I love you!"),
+  ];
+
+  const response = await chatModel.invoke(messages);
+  const formattedResponse = await praser.invoke(response);
+  console.log(formattedResponse);
+  return formattedResponse;
+};
